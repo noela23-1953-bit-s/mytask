920af62ff0c6718d0164cb91dd290b6d6182088c
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-31 12:19:30 +0300

    commit#2
    in the NV on line 10, an access modifier public was changed to protected.

--- TASK_10/src/NurseService.java
+++ TASK_10/src/NurseService.java
@@ -7,7 +7,7 @@ public class NurseService implements MedicalService{
     }
 
     @Override
-    public String ProvideCare(String patientName) {
+    protected String ProvideCare(String patientName) {
         System.out.println("Nurse + " + NurseName + " is providing care to " + patientName);
         return "Patient" + patientName + " has been treated by Nurse " + NurseName;
     }
