82a5b2cc27ffdb8b45399d0386cc85ea58155c08
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-31 11:29:07 +0300

    commit#2
    in the NV on line 5 to 8 a constructor was removed.

--- TASK_10/src/DoctorService.java
+++ TASK_10/src/DoctorService.java
@@ -2,10 +2,7 @@ public class DoctorService implements MedicalService{
     private String DoctorName;
     int DoctorId;
 
-    public DoctorService(String doctorName, int doctorId) {
-        DoctorName = doctorName;
-        DoctorId = doctorId;
-    }
+    
 
     @Override
     public String ProvideCare(String patientName) {
