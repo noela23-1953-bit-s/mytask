880f7ec5ff1afaad3396116a9fd0d8578639772c
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-31 12:39:15 +0300

    commit#2
    in the NV on line 5 a tab was added.

--- TASK_10/src/HospitalWard.java
+++ TASK_10/src/HospitalWard.java
@@ -2,6 +2,7 @@ public class HospitalWard {
     private MedicalService medicalService;
 
     public HospitalWard(MedicalService medicalService) {
+        
         this.medicalService = medicalService;
     }
     public final void admitPatient(String patientName){
